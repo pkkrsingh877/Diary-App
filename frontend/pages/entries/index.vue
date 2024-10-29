89fc51716dc647e19201b511143bfd480e32f3b9
Prabhat Kumar <pkkrsingh877@gmail.com>
2024-10-29 20:19:34 +0530

    entries page

--- frontend/pages/entries/index.vue
+++ frontend/pages/entries/index.vue
@@ -6,7 +6,7 @@
             </div>
             <div v-else>
                 <div v-for="entry in entries" :key="entry._id">
-                    <div class="card  mb-4">
+                    <div class="card mb-4">
                         <header class="card-header">
                             <p class="card-header-title">{{ entry.title }}</p>
                         </header>
@@ -16,7 +16,7 @@
                         <footer class="card-footer">
                             <a :href="`/entries/${entry._id}`" class="card-footer-item">View</a>
                             <a :href="`/entries/edit/${entry._id}`" class="card-footer-item">Edit</a>
-                            <a @click="deleteEntry(entry._id)" class="card-footer-item">Delete</a>
+                            <a @click.prevent="deleteEntry(entry._id)" class="card-footer-item">Delete</a>
                         </footer>
                     </div>
                 </div>
@@ -27,11 +27,12 @@
 
 <script setup>
 import { ref, onMounted } from 'vue';
+
 const entries = ref([]);
 
 const fetchEntries = async () => {
     try {
-        const response = await fetch(`/entries`, {
+        const response = await fetch('/entries', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -39,9 +40,18 @@ const fetchEntries = async () => {
             },
             credentials: 'include'
         });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
         const data = await response.json();
-        if (data.message !== 'Unauthorized: No token provided') {
+
+        // Ensure the response is valid JSON before assigning it to entries
+        if (Array.isArray(data)) {
             entries.value = data;
+        } else {
+            console.error('Unexpected response format:', data);
         }
     } catch (error) {
         console.error('Error fetching entries:', error);
@@ -60,13 +70,14 @@ const deleteEntry = async (id) => {
             },
             credentials: 'include'
         });
+
         if (response.ok) {
             entries.value = entries.value.filter(entry => entry._id !== id);
         } else {
-            console.error('Failed to delete todo. Server response:', response);
+            console.error('Failed to delete entry. Server response:', response);
         }
     } catch (error) {
-        console.error('Error deleting entries:', error);
+        console.error('Error deleting entry:', error);
     }
 };
 </script>

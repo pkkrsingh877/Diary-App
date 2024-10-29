4f00964dcc01e3a022f7179e6ccf097d87bcfa13
Prabhat Kumar <pkkrsingh877@gmail.com>
2024-10-29 19:57:46 +0530

    Allow all origins again: no

--- backend/index.js
+++ backend/index.js
@@ -16,13 +16,12 @@ app.use(express.json());
 app.use(methodOverride('_method'));
 app.use(morgan('dev'));
 app.use(cookieParser());
-app.use(cors());
-// {
-//     origin: '*',
-//         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-//             allowedHeaders: ['Content-Type', 'Authorization'],
-//                 credentials: true, // Allow credentials (cookies)
-// }
+app.use(cors({
+    origin: ['https://diary.prabhatkumar.site', 'https://www.diary.prabhatkumar.site'], // Replace with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // Allow credentials (cookies)
+}));
 
 // Middleware
 const verifyUser = require('./middlewares/verifyUser');

f8f19f7ac2061e45ac26a548cfee50418eb71d77
Prabhat Kumar <pkkrsingh877@gmail.com>
2024-10-29 19:51:35 +0530

    Allow all origins again?

--- backend/index.js
+++ backend/index.js
@@ -16,12 +16,13 @@ app.use(express.json());
 app.use(methodOverride('_method'));
 app.use(morgan('dev'));
 app.use(cookieParser());
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // Allow credentials (cookies)
-}));
+app.use(cors());
+// {
+//     origin: '*',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+//             allowedHeaders: ['Content-Type', 'Authorization'],
+//                 credentials: true, // Allow credentials (cookies)
+// }
 
 // Middleware
 const verifyUser = require('./middlewares/verifyUser');

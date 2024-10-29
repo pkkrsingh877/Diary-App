834d217dd21d8d9e0be9a5533141110afa8c0f0e
Prabhat Kumar <pkkrsingh877@gmail.com>
2024-10-29 18:54:20 +0530

    Origin Update

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(methodOverride('_method'));
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://diary.prabhatkumar.site',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // Allow credentials (cookies)
